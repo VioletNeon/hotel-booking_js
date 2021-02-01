c3f4c0c97600762565061c2ec271d6d03e90f41c
Valeriy Kulikov <KulikovVR@gmail.com>
2021-02-02 00:42:59 +0430

    Нужно больше функций. Устранен недочет в расчете второй функции при целых числах

--- js/main.js
+++ js/main.js
@@ -9,10 +9,10 @@ const getAnyNumber = function(from, to) {
 const getFractionalNumber = function(from, to, pointer) {
   let difference = Math.abs(to - from);
   if (from >= to) {
-    return +(Math.random() * (difference) + to).toFixed(pointer);
+    return +(+Math.random().toFixed(pointer) * (difference + Math.pow(10, -pointer)) + to).toFixed(pointer);
   }
-  return +(Math.random() * (difference) + from).toFixed(pointer);
+  return +(+Math.random().toFixed(pointer) * (difference + Math.pow(10, -pointer)) + from).toFixed(pointer);
 }
 
-alert(getAnyNumber(5, 7));
-alert(getFractionalNumber(5.5, 7.7, 3));
+alert(getAnyNumber(2, 7));
+alert(getFractionalNumber(2.1, 2.7, 2));
