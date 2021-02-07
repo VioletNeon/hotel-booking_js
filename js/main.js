388363a0d8a867674d351264c0d4e4b5a7bca049
Valeriy Kulikov <KulikovVR@gmail.com>
2021-02-08 00:08:11 +0430

    Больше деталей. Отработка по замечаниям 1

--- js/main.js
+++ js/main.js
@@ -1,57 +1,61 @@
-const getFractionalNumber = function(from, to, pointer = 0) {
+const TITLES = ['куплю', 'сниму', 'обменяю', 'продам', 'отдам', 'подарю', 'заберу', 'сдам', 'арендую', 'приму'];
+const TYPES = ['palace', 'flat', 'house', 'bungalow'];
+const CHECKINS = ['12:00', '13:00', '14:00'];
+const CHECKOUTS = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const DESCRIPTIONS = ['просторный', 'уютный', 'чистый', 'милый', 'красочный', 'широкий', 'классический', 'стильный', 'брутальный', 'модный'];
+const PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+const ID_AVATAR_FROM = 1;
+const ID_AVATAR_TO = 8;
+const COORDINATE_X_FROM = 35.65000;
+const COORDINATE_X_TO = 35.70000;
+const COORDINATE_Y_FROM = 139.70000;
+const COORDINATE_Y_TO = 139.80000;
+const NUMBER_AFTER_POINT = 5;
+const PRICE_ITEM_FROM = 0;
+const PRICE_ITEM_TO = 1000;
+const ROOMS_NUMBER_FROM = 1;
+const ROOMS_NUMBER_TO = 5;
+const GUESTS_NUMBER_FROM = 1;
+const GUESTS_NUMBER_TO = 10;
+const SOME_AD_COUNT = 10;
+
+const getFractionalNumber = function (from, to, pointer = 0) {
   [from, to, pointer] = [Math.abs(from), Math.abs(to), Math.abs(pointer)];
   if (from >= to) {
     [from, to] = [to, from];
   }
   return +(Math.random() * Math.abs(to - from) + from).toFixed(pointer);
-};
-
-const TITLE = ['куплю', 'сниму', 'обменяю', 'продам', 'отдам', 'подарю', 'заберу', 'сдам', 'арендую', 'приму'];
-const TYPE = ['palace', 'flat', 'house', 'bungalow'];
-const CHECKIN = ['12:00', '13:00', '14:00'];
-const CHECKOUT = ['12:00', '13:00', '14:00'];
-const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const DESCRIPTION = ['просторный', 'уютный', 'чистый', 'милый', 'красочный', 'широкий', 'классический', 'стильный', 'брутальный', 'модный'];
-const PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-
-const createSomeArray = (array) => {
-  let ARRAY = array.filter((item) => {
-    if (getFractionalNumber(0, 1) === 1) {
-      return item
-    }
-  });
-  if (ARRAY.length === 0) {
-    ARRAY = createSomeArray(array)
-  }
-  return ARRAY
 }
 
-const SOME_AD_COUNT = 10;
+const getRandomArray = function (array) {
+  return array.sort(function() {return Math.random() - 0.5}).slice(0, getFractionalNumber(0, array.length - 1))
+}
 
 const getRandomElements = function (elements) {
   return elements[getFractionalNumber(0, elements.length - 1)]
 }
 
-const createSomeDescriptionAd = () => {
-  const author = {
-    avatar: 'img/avatars/user' + '0' + getFractionalNumber(1, 8) + '.png',
+const createSomeDescriptionAd = function () {
+  let author = {
+    avatar: 'img/avatars/user' + '0' + getFractionalNumber(ID_AVATAR_FROM, ID_AVATAR_TO) + '.png',
   };
-  const location = {
-    x: getFractionalNumber(35.65000, 35.70000, 5),
-    y: getFractionalNumber(139.70000, 139.80000, 5),
+  let location = {
+    x: getFractionalNumber(COORDINATE_X_FROM, COORDINATE_X_TO, NUMBER_AFTER_POINT),
+    y: getFractionalNumber(COORDINATE_Y_FROM, COORDINATE_Y_TO, NUMBER_AFTER_POINT),
   };
-  const offer = {
-    title: getRandomElements(TITLE),
+  let offer = {
+    title: getRandomElements(TITLES),
     address: location.x + ', ' + location.y,
-    price: getFractionalNumber(0, 1000),
-    type: getRandomElements(TYPE),
-    rooms: getFractionalNumber(1, 5),
-    guests: getFractionalNumber(1, 10),
-    checkin: getRandomElements(CHECKIN),
-    checkout: getRandomElements(CHECKOUT),
-    features: createSomeArray(FEATURES),
-    description: getRandomElements(DESCRIPTION),
-    photos: createSomeArray(PHOTOS),
+    price: getFractionalNumber(PRICE_ITEM_FROM, PRICE_ITEM_TO),
+    type: getRandomElements(TYPES),
+    rooms: getFractionalNumber(ROOMS_NUMBER_FROM, ROOMS_NUMBER_TO),
+    guests: getFractionalNumber(GUESTS_NUMBER_FROM, GUESTS_NUMBER_TO),
+    checkin: getRandomElements(CHECKINS),
+    checkout: getRandomElements(CHECKOUTS),
+    features: getRandomArray(FEATURES),
+    description: getRandomElements(DESCRIPTIONS),
+    photos: getRandomArray(PHOTOS),
   };
   return {
     author,
@@ -60,6 +64,6 @@ const createSomeDescriptionAd = () => {
   }
 }
 
-const someDescriptionAd = new Array(SOME_AD_COUNT).fill(null).map(() => createSomeDescriptionAd());
+const someDescriptionAd = new Array(SOME_AD_COUNT).fill(null).map(function() {return createSomeDescriptionAd()});
 
 alert(someDescriptionAd);
